c233020f7d6a9eeb47775575f287253ac7367108
EDCA <eric.cavanzo@outlook.com>
2025-08-06 16:39:06 -0500

    feat(): Crear entidades de `Users` y `Profile` para la gestión de usuarios
    Se implementan las entidades `Users` y `Profile` utilizando TypeORM para definir la estructura de datos y las relaciones de la base de datos, sentando las bases para la gestión de usuarios.
    
    La entidad `Users` incluye:
    - Campos básicos para autenticación como `userPassword` y `userEmail`.
    - Un índice para `userEmail` para optimizar consultas de búsqueda.
    
    La entidad `Profile` incluye:
    - Campos para información detallada del perfil como `profileName`, `profileLastname`, `profileEmail` y `profileAvatar`.
    - Una relación `OneToOne` con la entidad `Users` para vincular cada perfil a un usuario.
    - Índices para `userEmail` y un `JoinColumn` en `user_id` para gestionar la relación.
    
    Esta implementación separa los datos de autenticación (`Users`) de la información pública del perfil (`Profile`), lo que mejora la escalabilidad y la seguridad del sistema.

---
+++ src/profile/entities/profile.entity.ts
@@ -0,0 +1,58 @@
+import { Users } from 'src/users/entities/user.entity';
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('profile')
+@Index(['userEmail'])
+export class User extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({
+		name: 'profile_name',
+		type: 'varchar',
+		length: 255,
+		comment: 'Nombre del usuario',
+	})
+	profileName: string;
+
+	@Column({
+		name: 'profile_lastname',
+		type: 'varchar',
+		length: 255,
+		comment: 'apellido del usuario',
+	})
+	profileLastname: string;
+
+	@Column({
+		name: 'profile_email',
+		type: 'varchar',
+		length: 255,
+		comment: 'Email del usuario, unico por usuario',
+		unique: true,
+	})
+	profileEmail: string;
+
+	@Column({
+		name: 'profile_avatar',
+		type: 'varchar',
+		length: 300,
+		comment: 'URL del avatar del usuario',
+	})
+	profileAvatar: string;
+
+	@OneToOne(() => Users)
+	@JoinColumn({
+		name: 'user_id',
+	})
+	userId: Users;
+
+	@CreateDateColumn({
+		name: 'created_at',
+	})
+	createdAt: Date;
+
+	@UpdateDateColumn({
+		name: 'updated_at',
+	})
+	updatedAt: Date;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+@Index(['userEmail'])
+export class Users extends BaseEntity {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({
+		name: 'user_password',
+		type: 'varchar',
+		length: 255,
+		comment: 'Nombre del usuario',
+	})
+	userPassword: string;
+
+	@Column({
+		name: 'user_email',
+		type: 'varchar',
+		length: 255,
+		comment: 'Email del usuario, unico por usuario',
+		unique: true,
+	})
+	userEmail: string;
+
+	@CreateDateColumn({
+		name: 'created_at',
+	})
+	createdAt: Date;
+
+	@UpdateDateColumn({
+		name: 'updated_at',
+	})
+	updatedAt: Date;
+}
