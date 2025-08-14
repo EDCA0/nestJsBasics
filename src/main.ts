23703989fb431c6e22dc15940ffbfc6aee505ae8
EDCA <eric.cavanzo@outlook.com>
2025-08-14 14:01:22 -0500

    feat(swagger): Añadir documentación de la API con Swagger Se configura Swagger para generar y servir la documentación de la API, facilitando a los desarrolladores la exploración y el uso de los endpoints.
    Los principales cambios incluyen:
    - Instalar las dependencias `@nestjs/swagger` y `swagger-ui-express`.
    - Importar `DocumentBuilder` y `SwaggerModule` en `src/main.ts`.
    - Configurar el título, la descripción, la versión y la autenticación de la API.
    - Generar el documento de Swagger y exponerlo en la ruta `/docs`.
    - Establecer una ruta personalizada para el archivo JSON de la documentación en `swagger/json`.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -12,6 +13,13 @@ async function bootstrap() {
 			transform: true,
 		}),
 	);
+
+	const config = new DocumentBuilder().setTitle('Blog API').setDescription('Blog API description').setVersion('1.0').addBearerAuth().build();
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('docs', app, documentFactory, {
+		jsonDocumentUrl: 'swagger/json',
+	});
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
