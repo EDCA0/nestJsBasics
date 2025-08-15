01f10ce5bf60db147d2f3894580c11331faf1610
EDCA <eric.cavanzo@outlook.com>
2025-08-14 20:18:27 -0500

    feat(): Configurar seguridad de la aplicación con helmet y CORS
    Se añade la librería `helmet` para proteger la aplicación de vulnerabilidades web comunes y se configura el middleware CORS para permitir peticiones desde cualquier origen, facilitando el desarrollo con frontends en diferentes dominios.
    
    Adicionalmente, se actualiza la versión de `node` en `package.json` para reflejar la versión usada en el proyecto.
    - Instalación de la librería `helmet` y su inclusión en `package.json` y `package-lock.json`.
    - Integración de `helmet` en `src/main.ts` para habilitar un conjunto de cabeceras de seguridad HTTP.
    - Configuración de CORS en `src/main.ts` para permitir el acceso desde múltiples orígenes, con un comentario clarificador sobre su uso en producción.
    - Ajuste de la versión de `node` en `package.json` a `22.18.0`.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -18,6 +19,13 @@ async function bootstrap() {
 	const documentFactory = () => SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup('docs', app, documentFactory, {
 		jsonDocumentUrl: 'swagger/json',
+	});
+
+	app.use(helmet());
+
+	app.enableCors({
+		origin: '*', //> Aca se pone el origen o el dominio, en caso de que sea tambien para moviles lo usual es poner * pero en caso contrario
+		//> Que solo sea web entonces lo mejor es poner la direccion concreta
 	});
 
 	await app.listen(process.env.PORT ?? 3000);
